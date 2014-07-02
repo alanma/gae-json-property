50f89d26eaf57d9dca077eea26a862eebc34338f
Matthew Belisle <matthew.belisle@webfilings.com>
2014-07-02 11:58:05 -0600

    Adding jsonproperty

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-datastore-jsonproperty
+jsonproperty
 ======================
 
-JsonProperty for Go Appengine Datastore
+A datastore JsonProperty in Go for Google App Engine.  Equivalent to the Python
+[ndb.JsonProperty](https://developers.google.com/appengine/docs/python/ndb/properties).
---
+++ jsonproperty.go
@@ -0,0 +1,68 @@
+package jsonproperty
+
+import (
+	"appengine/datastore"
+	"encoding/json"
+	"reflect"
+)
+
+type JsonProperty map[string]interface{}
+
+var jsonPropertyType = reflect.TypeOf(JsonProperty{})
+
+// entity must be a pointer to a struct
+func LoadJsonProperties(entity interface{}, c <-chan datastore.Property) (<-chan datastore.Property, error) {
+	jsonProperties := map[string]reflect.Value{} // {name: value}
+
+	// Builds jsonProperties
+	value := reflect.ValueOf(entity).Elem()
+	for i := 0; i < value.NumField(); i++ {
+		value2 := value.Field(i)
+		if value2.Type() == jsonPropertyType {
+			jsonProperties[getNameFromField(value.Type().Field(i))] = value2
+		}
+	}
+
+	// Builds return channel
+	c2 := make(chan datastore.Property, value.NumField()-len(jsonProperties))
+	defer close(c2)
+	for property := range c {
+		if jsonValue, ok := jsonProperties[property.Name]; ok {
+			bytes := []byte(property.Value.(string))
+			if err := json.Unmarshal(bytes, jsonValue.Addr().Interface()); err != nil {
+				return c2, err
+			}
+		} else {
+			c2 <- property
+		}
+	}
+
+	return c2, nil
+}
+
+// entity must be a pointer to a struct
+func SaveJsonProperties(entity interface{}, c chan<- datastore.Property) (chan<- datastore.Property, error) {
+	value := reflect.ValueOf(entity).Elem()
+	for i := 0; i < value.NumField(); i++ {
+		value2 := value.Field(i)
+		if value2.Type() == jsonPropertyType {
+			bytes, err := json.Marshal(value2.Interface())
+			if err != nil {
+				return c, err
+			}
+			c <- datastore.Property{
+				Name:  nameFromField(value.Type().Field(i)),
+				Value: string(bytes),
+			}
+		}
+	}
+
+	return c, nil
+}
+
+func nameFromField(f *reflect.Field) string {
+	if name := f.Tag.Get("jsonproperty"); name != "" {
+		return name
+	}
+	return f.Name
+}
